424b446ec6f82b232fcff66ecff99d8dd2172ec1
phiroun <man.phiroun@gmail.com>
2023-04-24 14:08:46 -0700

    new codes

--- greater-than5.js
+++ greater-than5.js
@@ -1,4 +1,12 @@
 //Using if/else statements, JavaScript code in a script tag that prints true if both parameters are greater or equal to 5.
 
-const num1 = 5;
-const num2 = 6;
+const num1 = 0;
+const num2 = 0;
+const sum = num1 + num2;
+if (sum > 5) {
+  console.log("true");
+} else if (sum < 5) {
+  console.log("fales");
+} else if (sum === 5) {
+  console.log("true");
+}
--- pair-and-compare.js
+++ pair-and-compare.js
@@ -1,4 +1,11 @@
 //Using if/else statements and strict equality operators, JavaScript code in a script tag that compares two sets of two values. Print true if at least one of the pairs is truthy.
 
-const param1A = "cat";
-const param1B = "cat";
+const param1A = 0;
+const param1B = false;
+const param2A = "horse";
+const param2B = "horse";
+if (param1A === param1B || param2A === param2B){
+    console.log("true");
+} else if (param1A !== param1B || param2A !== param2B){
+    console.log("false");
+}
